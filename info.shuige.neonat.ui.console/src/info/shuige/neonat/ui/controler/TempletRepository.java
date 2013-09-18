486bc3efa64920b31f1a6f94598997860253a641
elaineloop <elaineloop@gmail.com>
2013-09-18 23:43:26 +0800

    ui

---
+++ info.shuige.neonat.ui.console/src/info/shuige/neonat/ui/controler/TempletRepository.java
@@ -0,0 +1,9 @@
+package info.shuige.neonat.ui.controler;
+
+public class TempletRepository {
+	public static final String BOARD_PAGE = "BOARD_PAGE"; 
+	public static final String HELP_PAGE = "HELP_PAGE"; 
+	public static final String TOPIC_PAGE = "TOPIC_PAGE"; 
+	public static final String WELCOME_PAGE = "WELCOME_PAGE"; 
+	public static final String PROMPT = "PROMPT"; 
+}
